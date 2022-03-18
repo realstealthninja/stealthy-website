5a3c15e2067a27abb34bb0413d022c76b1ef5334
stealthninja <realstealthninja@gmail.com>
2022-03-18 20:42:04 +0530

    making nav bar button more mobile friendly

--- components/index/navBarBtn.tsx
+++ components/index/navBarBtn.tsx
@@ -5,9 +5,9 @@ interface NavBarBtnInterface {
 
 const NavBarBtn = ({Name, Link} : NavBarBtnInterface) => {
     return (
-        <li className="nav-item">
+        <li className="nav-item border-solid lg:m-2 ">
                 <a
-                  className=" nav-link block py-2 pr-2 text-gray-600 transition duration-150 ease-in-out hover:text-gray-700 focus:text-gray-700 lg:px-2"
+                  className="lg:px-2 nav-link block py-2 pr-2 text-gray-600 transition duration-150 ease-in-out hover:text-gray-700 focus:text-gray-700"
                   href={Link}
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import NavBar from "../components/index/navBar"
+import ArticleBox from "../components/index/articleBox"
 
 const buttons = ["Home", "About", "Projects", "Contact"]
 const links = ["/", "/about", "/projects", "/contact"]
@@ -13,21 +14,66 @@ const Home: NextPage = () => {
           <title>stealthy website</title>
         </Head>
         <NavBar Buttons={buttons} Links={links}></NavBar>
-        <div className="relative h-[350px] overflow-hidden bg-[url('https://wallpaper.dog/large/20366415.jpg')] bg-cover bg-no-repeat">
-          <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed">
-            <div className="flex justify-center items-center h-full">
-              <div className="text-center text-white px-6 md:px-12">
-                <h1 className="text-5xl font-bold mt-0 mb-6">Welcome</h1>
-                <h3 className="text-3xl font-bold mb-8"> To my website </h3>
-                <button className="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-            data-mdb-ripple="true" data-mdb-ripple-color="light">Get started</button>
+        <div className="relative h-[350px] overflow-hidden bg-[url('https://www.acemetrix.com/wp-content/uploads/2015/08/black-banner-vector3.png')] bg-cover bg-no-repeat">
+          <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed">
+            <div className="flex h-full items-center justify-center">
+              <div className="px-6 text-center text-white md:px-12">
+                <h1 className="mt-0 mb-6 text-5xl font-bold">Welcome</h1>
+                <h3 className="mb-8 text-3xl font-bold"> To my website </h3>
+                <button
+                  className="inline-block rounded border-2 border-white px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  Get started
+                </button>
               </div>
             </div>
           </div>
         </div>
       </header>
       <body className="dark: bg-Ebony">
-
+        <div className="flex-col m-10 flex lg:flex-row">
+          <div className="mt-10 lg:mr-10  basis-1/3">
+            <ArticleBox
+              Title="Stealthy bot"
+              Mainbody="One of my most worked on projects i've been at developing stealthy bot for about six months now "
+              ImageUrl="https://cdn.discordapp.com/avatars/889922820317007928/9182f4cfa68a27628dc9927fd1459b93.png?size=128"
+              ImageAlt="Stealthy bot image"
+              ButtonName="Github"
+              ButtonOnClick={() => {
+                location.href =
+                  "https://github.com/realstealthninja/Stealthybot"
+              }}
+            ></ArticleBox>
+          </div>
+          <div className="mt-10 lg:mr-10  basis-1/3">
+            <ArticleBox
+              Title="This website"
+              Mainbody="Thats right! this website is completely open source! isnt that wonderful? check it out!"
+              ImageUrl="https://3.bp.blogspot.com/-vd3HKLEsew0/T7uQM1U3BuI/AAAAAAAAAgk/12Nu4wEc8ZA/s1600/amy-lee-black-dark.jpg"
+              ImageAlt="Stealthy bot image"
+              ButtonName="Github"
+              ButtonOnClick={() => {
+                location.href =
+                  "https://github.com/realstealthninja/Stealthybot"
+              }}
+            ></ArticleBox>
+          </div>
+          <div className="mt-10 lg:mr-10  basis-1/3">
+            <ArticleBox
+            Title="My Github"
+            Mainbody="I have a github account. In which all my open source projects lay wide open for you too catch a glipse of, Check it out."
+            ImageUrl="https://www.jpihealthcare.com/wp-content/uploads/2017/01/Banner1-Background_dark-1024x381.jpg"
+            ImageAlt="PlaceHolder"
+            ButtonName="GitHub"
+            ButtonOnClick={() => {
+              location.href= 
+                "https://github.com/realstealthninja"
+            }} 
+            ></ArticleBox>
+          </div>
+        </div>
       </body>
     </>
   )

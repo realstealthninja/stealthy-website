3d6578482b9777a4a3bb684cf5e0088761a0c8b8
realstealthninja <realstealthninja@gmail.com>
2022-08-09 12:00:21 +0530

    refactor: basically everything

---
+++ src/components/home/card.tsx
@@ -0,0 +1,31 @@
+import { NextComponentType } from "next";
+import { Dispatch } from "react";
+
+
+interface HomeCardInterface {
+    text: string;
+    name: string;
+    banner: string;
+    onClickEvent: Dispatch<any>;
+}
+
+
+const HomeCard = (
+    { name, text, banner, onClickEvent }: HomeCardInterface) => {
+    return (
+        <div className="ml-5 mt-5 max-w-sm rounded-md overflow-hidden shadow-lg bg-slate-900 basis-1/3">
+            <img className="w-full object-contain" src={banner}/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{name}</div>
+                <p className="text-gray-700 text-base">
+                    {text}
+                </p>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 text-sm rounded" onClick={onClickEvent}>Go to</button>
+            </div>
+            
+        </div>
+
+    );
+};
+
+export default HomeCard;
---
+++ src/pages/index.tsx
@@ -0,0 +1,73 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Typewriter from "typewriter-effect"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons"
+import Link from "next/link"
+import Image from "next/image"
+import HomeCard from "../components/home/card"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>stealthy website</title>
+        <link rel="icon" href="/stealthysmile.ico"/>
+      </Head>
+      <div className="bg-black text-white h-screen scrollbar scrollbar-thumb-black scrollbar-track-gray-900">
+        <div>
+          <ul>
+            <li className="inline ml-5 mt-5 fa-github">
+              <Link href="https://github.com/realstealthninja">
+                <FontAwesomeIcon icon={faGithub} size="lg" />
+              </Link>
+            </li>
+            <li className="inline ml-5 mt-5">
+              <FontAwesomeIcon icon={faInstagram} size="lg" />
+            </li>
+            <li className="inline ml-5 mt-5">
+              <Link href="https://www.youtube.com/channel/UCRBu1hybrrioa8hZc5_Ggig">
+                <FontAwesomeIcon icon={faYoutube} size="lg" />
+              </Link>
+            </li>
+            <li className="float-right mr-5"><Image src="/stealthysmile.png" width="30px" height="10px" /></li>
+          </ul>
+        </div>
+        <div className="text-5xl flex items-center justify-center h-screen bg-black">
+          <Typewriter options={{
+            strings: ["Programming", "Gaming", "Designing", "[{Stealthy.dev}]", "stealthninja"],
+            autoStart: true,
+            loop: true,
+          }} />
+        </div>
+        <div className="text-4xl text-center bg-black">
+          <h1>Projects</h1>
+          <div className="flex flex-col md:flex-row">
+
+            <HomeCard
+              name="stealthy bot"
+              banner="/stealthybot.png"
+              text="stealthy bot is a discord general bot, it has many features. This was the first project ive ever worked on very hard. still alot more to do tho."
+              onClickEvent={
+                () => {
+                  window.location.replace("https://github.com/realstealthninja/Stealthybot");
+                }}
+            />
+            <HomeCard
+            name="stealthy website"
+            banner="/website.png"
+            text="the project im very proud to start, this has been a long time coming. My first ever project in nextjs + tailwind + typescript"
+            onClickEvent={
+              () => {
+                  window.location.replace("https://github.com/realstealthninja/stealthy-website")
+              }
+            }/>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
